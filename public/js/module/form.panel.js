13998434804b5b4e107fc5b89df47edcce04ee4b
xjq熊剑桥 <jqxiong@Ctrip.com>
2017-03-23 10:41:13 +0800

    form panel model

--- public/js/module/form.panel.js
+++ public/js/module/form.panel.js
@@ -2,11 +2,25 @@
 define(['backbone', 'text!template/form.panel.html'],
 	function(backbone, Template) {
 
+		var Model = Backbone.Model.extend({
+			defaults: {
+				title: '加班打车',
+				content: '项目发布',
+				by: {
+					'0': '出租'
+				},
+				toTime: '',
+				casePrice: 100,
+				totalPrice: 100
+			}
+		});
+
 		var View = Backbone.View.extend({
 			el: '#j_form_panel',
 			template: _.template(Template),
+			model: new Model(),
 			render: function() {
-				this.$el.html(this.template())
+				this.$el.html(this.template(this.model.toJSON()))
 			},
 			setOptions: function() {}
 		});
